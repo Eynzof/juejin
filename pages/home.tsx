2fe03124753fabc7334447727c5b3bb51e7f1167
Eynzo Li <Eynzof@gmail.com>
2023-01-18 20:27:02 +0800

    responsive homepage

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,20 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import styles from './Home.module.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "./Home.module.css";
 // import { Article } from './types';
 
-interface Props {
-
-}
+interface Props {}
 
 const Home: React.FC<Props> = () => {
   // const [articles, setArticles] = useState<Article[]>([]);
   const [headerCollapsed, setHeaderCollapsed] = useState(false);
 
+  const words = [
+    "首页",
+    "沸点",
+    "课程上新",
+    "直播",
+    "活动",
+    "竞赛",
+    "商城",
+    "APP",
+    "插件",
+  ];
+
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -29,15 +39,36 @@ const Home: React.FC<Props> = () => {
   return (
     <div className={styles.home__container}>
       <header className={styles.header}>
-        <div className={`${styles.header__top} ${headerCollapsed ? styles.collapsed : ''}`}>
+        <div
+          className={`${styles.header__top} ${
+            headerCollapsed ? styles.collapsed : ""
+          }`}
+        >
           {/* header top content */}
           <div className={styles.header__container}>
-            <img src="/juejin_logo.svg" alt="Logo" className={styles.header__logo} />
+            <img
+              src="/juejin_logo.svg"
+              alt="Logo"
+              className={styles.header__logo}
+            />
             <div className={styles.header__navigation}>
-              <ul className={styles.header__navlist}>
-                <li>首页</li>
-                <li></li>
-              </ul>
+              <div className={styles.header__navlist}>
+                {words.map((word, index) => (
+                  <div key={index} className={styles.header__navkey}>
+                    <a href="" className={styles.header__navlink}>
+                      {word}
+                    </a>
+                  </div>
+                ))}
+                <div className={styles.header__banner}>
+                  <img
+                    src="/header_banner.jpg"
+                    alt="Logo"
+                    style={{ maxWidth: 115, maxHeight: 40 }}
+                  />
+                </div>
+              </div>
+              <div className={styles.header__rightside}>aaa</div>
             </div>
           </div>
         </div>
@@ -47,7 +78,7 @@ const Home: React.FC<Props> = () => {
         </div>
       </header>
       <main className={styles.main}>
-        <div className={styles.main__left} style={{ width: '75%' }}>
+        <div className={styles.main__left}>
           <div>
             <h1>Articles</h1>
             <p>Article lore</p>
@@ -96,17 +127,13 @@ const Home: React.FC<Props> = () => {
           {/*  </div>*/}
           {/*))}*/}
         </div>
-        <div className={styles.main__right} style={{ width: '25%' }}>
-          <div className={styles.author__info}>
-
-          </div>
+        <div className={styles.main__right}>
+          <div className={styles.author__info}></div>
           <div className={styles.related__articles}>
             <h4>Related Articles</h4>
-
           </div>
           <div className={styles.table__of__contents}>
             <h4>Table of Contents</h4>
-
           </div>
         </div>
       </main>
