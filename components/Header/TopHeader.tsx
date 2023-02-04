2889f305332b40f27e9259f3c9bc030920ffffaa
Eynzo Li <Eynzof@gmail.com>
2023-02-04 14:41:06 +0800

    feat: add responsive menu

--- components/Header/TopHeader.tsx
+++ components/Header/TopHeader.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Box, Link, useMediaQuery, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  Link,
+  Menu,
+  MenuItem,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import styles from "./TopHeader.module.css";
 import JueJinLogo from "./JueJinLogo";
 import Operations from "./Operations";
@@ -13,24 +21,75 @@ const TopHeader = () => {
     menus_result.data && menus_result.data.menu.data.attributes.data;
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
-  console.log(matches);
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className={styles.header__container}>
       <JueJinLogo />
       <div className={styles.header__navigation}>
+        <div className={styles.header_navcol}>
+          <Button
+            id="demo-positioned-button"
+            aria-controls={open ? "demo-positioned-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            首页
+          </Button>
+          <Menu
+            id="demo-positioned-menu"
+            aria-labelledby="demo-positioned-button"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+          >
+            {menus &&
+              menus.map((menu, index) => (
+                <Box sx={{ color: "text.default" }} key={index}>
+                  <Link
+                    href={menu.url}
+                    className={styles.header__navlink}
+                    sx={{ color: "text.secondary" }}
+                  >
+                    {menu.name}
+                  </Link>
+                </Box>
+              ))}
+          </Menu>
+        </div>
         <div className={styles.header__navlist}>
-          {menus &&
-            menus.map((menu, index) => (
-              <Box sx={{ color: "text.default" }} key={index}>
-                <Link
-                  href={menu.url}
-                  className={styles.header__navlink}
-                  sx={{ color: "text.secondary" }}
-                >
-                  {menu.name}
-                </Link>
-              </Box>
-            ))}
+          <div className={styles.header__nav}>
+            {menus &&
+              menus.map((menu, index) => (
+                <Box sx={{ color: "text.default" }} key={index}>
+                  <Link
+                    href={menu.url}
+                    className={styles.header__navlink}
+                    sx={{ color: "text.secondary" }}
+                  >
+                    {menu.name}
+                  </Link>
+                </Box>
+              ))}
+          </div>
+
           <div className={styles.header__banner}>
             <img
               src="/header_banner.jpg"
